7479f28256655aa2537c1b8bb028b5958106df75
Mitchell Read <fox_mitchell1@hotmail.com>
2019-11-12 13:17:45 -0800

    started datamuse api and using wikipedia for links

--- Wiki_Race/wiki_race.py
+++ Wiki_Race/wiki_race.py
@@ -1,140 +1,56 @@
 import urllib.request
 import urllib.error
 from bs4 import BeautifulSoup
+from datamuse_api import Datamuse
 import wikipedia
+import json
 
 
 def main():
-    main_url = 'https://en.wikipedia.org/'
+    start_word = 'bananas'
+    end_word = 'apples'
+    url_attempts = 20
 
-    start = 'wiki/bananas'
-    # start = 'wiki/fruit'
-    end = 'wiki/Apples'
-    url_attempts = 2
-
-    soup = get_body_content(main_url + start)
-
-    end_target_links = find_links(soup, 'apples')
-
-    if end_target_links != []:
-        # navigate the links
-        return
-
-    all_valid_links = find_links(soup)
-
-    for link in all_valid_links:
-        print(link)
-
-    return
-    if all_valid_links != []:
-
-        return 
-    else:
-        print(f'Could not find a connection between {start} and {end}.')
+    results = begin_wiki_hunt(start_word, end_word, url_attempts)
 
     return
 
 
-# def navigate_links(links, depth, )
+def begin_wiki_hunt(start_word, end_word, url_attempts):
+    main_url = 'https://en.wikipedia.org/wiki/'
+    datamuse = Datamuse()
+
+    results = {
+        'start_word': start_word,
+        'end_word': end_word,
+        'found_path': [],
+        'successful': False,
+    }
+
+    # start_links = wikipedia.WikipediaPage(start_word).links 
+    # end_links = wikipedia.WikipediaPage(end_word).links
+
+    # start_links = set(start_links)
+    # end_links = set(end_links)
+
+    # link_intersects = start_links.intersection(end_links)
+
+    with open('same_links.txt') as f:
+        link_interects = f.read().splitlines()
 
 
+    # start_similar = datamuse.get_similar_meaning(start_word)
+    # end_similar = datamuse.get_similar_meaning(end_word)
 
-def check_winning_page(end_target, url):
-    '''
-    Used to decide if we have reached the end target page by providing the url. 
-    We split the URL on / and grab the last word in the list and compare to our end target word that is sentence capitalized (wiki specific).
-    Next it will grab the first body content url in the case of a redirect (still valid) and compare to that.
-        i.e. apples redirects to apple in the wiki url and will be counted as valid.
+    with open('start_sim.json') as f:
+        start_similar = json.load(f)
+
+    with open('end_sim.json') as f:
+        end_similar = json.load(f)
 
     
-    Args:
-        end_target (str): Target page we want to end up on
-        url (str): Wiki valid URL
-    
-    Returns:
-        bool: True if we believe to have found the page and false otherwise
-    '''
-    url_word = url.split('/')
-    url_word = url_word[-1]
-    end_target = end_target.capitalize()
 
-    if url_word == end_target:
-        return True 
-
-    soup = get_body_content(url)
-    links = find_links(soup, wiki_only=False)
-    links = links[0]
-
-    if end_target in links and 'redirect' in links:
-        return True 
-
-    return False
-
-
-def get_body_content(url):
-    '''
-    Gets the body contents of a wikipedia page
-    
-    Args:
-        url (str): URL to a wiki page
-    
-    Returns: 
-        bs4 object: bs4 object contained the body content HTML data of the wiki page
-        False: Occurs if the page fails to open or does not exist
-    '''
-    page = open_page(url)
-
-    if page == False:
-        return False
-
-    soup = BeautifulSoup(page, 'html.parser')
-
-    for div in soup.find_all('div', {'class': 'reflist'}):
-        div.decompose()
-
-    soup = soup.find(id='bodyContent')
-
-    return soup
-
-
-def find_links(soup, find_me='', wiki_only=True):
-    '''
-    Finds all links with the find_me keyword in them using Beautiful soup
-    
-    Args:
-        soup (bs4 object): A beautiful soup object
-        find_me (str): Keyword to find. Defaults to find all links.
-        wiki_only (bool): Grabs only the links that start with /wiki/
-    
-    Returns:
-        list: List of links found in the search
-    '''
-    if wiki_only:
-        return [link['href'] for link in soup.find_all('a', href=True, string=find_me) if link['href'].startswith('/wiki/') and ':' not in link['href'] and '#' not in link['href']]
-    else :
-        return [link['href'] for link in soup.find_all('a', href=True, string=find_me) if ':' not in link['href'] and '#' not in link['href']]
-
-
-def open_page(url):
-    '''
-    The following function will open a URL page and read its contents to extract data.
-    
-    Args:
-        url (str): URL to a webpage
-    
-    Returns:
-        str: Webpage contents of provided URL
-    '''
-    try:
-        url_site = urllib.request.urlopen(url)
-    except urllib.error.HTTPError as e:
-        print(f'{e.code} error - {e.reason} -- Skipping: {url} ')
-        return False
-    except urllib.request.URLError as e:
-        print(f'Please confirm {url} is a proper URL.')
-        return False
-
-    return url_site.read()#.decode('utf-8')
+    return results
 
 
 if __name__ == "__main__":
