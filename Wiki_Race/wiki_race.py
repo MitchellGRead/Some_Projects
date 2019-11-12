390945dcc97fabd5ae9b12379a9391beaf57b323
Mitchell Read <fox_mitchell1@hotmail.com>
2019-11-11 18:40:30 -0800

    added winning checks and better link grabbing

--- Wiki_Race/wiki_race.py
+++ Wiki_Race/wiki_race.py
@@ -1,6 +1,7 @@
 import urllib.request
 import urllib.error
 from bs4 import BeautifulSoup
+import wikipedia
 
 
 def main():
@@ -8,25 +9,23 @@ def main():
 
     start = 'wiki/bananas'
     # start = 'wiki/fruit'
-    end = 'wiki/apples'
+    end = 'wiki/Apples'
+    url_attempts = 2
 
-    page = open_page(main_url + start)
+    soup = get_body_content(main_url + start)
 
-    soup = BeautifulSoup(page, 'html.parser')
-
-    for div in soup.find_all('div', {'class': 'reflist'}):
-        div.decompose()
-
-    soup = soup.find(id='bodyContent')
-
-    end_target_links = [link['href'] for link in soup.find_all('a', href=True, string='apples') if ':' not in link['href'] and '#' not in link['href']]
+    end_target_links = find_links(soup, 'apples')
 
     if end_target_links != []:
         # navigate the links
         return
 
-    all_valid_links = [link['href'] for link in soup.find_all('a', href=True) if ':' not in link['href'] and '#' not in link['href']]
+    all_valid_links = find_links(soup)
 
+    for link in all_valid_links:
+        print(link)
+
+    return
     if all_valid_links != []:
 
         return 
@@ -36,8 +35,84 @@ def main():
     return
 
 
-def navigate_link(url):
-    return
+# def navigate_links(links, depth, )
+
+
+
+def check_winning_page(end_target, url):
+    '''
+    Used to decide if we have reached the end target page by providing the url. 
+    We split the URL on / and grab the last word in the list and compare to our end target word that is sentence capitalized (wiki specific).
+    Next it will grab the first body content url in the case of a redirect (still valid) and compare to that.
+        i.e. apples redirects to apple in the wiki url and will be counted as valid.
+
+    
+    Args:
+        end_target (str): Target page we want to end up on
+        url (str): Wiki valid URL
+    
+    Returns:
+        bool: True if we believe to have found the page and false otherwise
+    '''
+    url_word = url.split('/')
+    url_word = url_word[-1]
+    end_target = end_target.capitalize()
+
+    if url_word == end_target:
+        return True 
+
+    soup = get_body_content(url)
+    links = find_links(soup, wiki_only=False)
+    links = links[0]
+
+    if end_target in links and 'redirect' in links:
+        return True 
+
+    return False
+
+
+def get_body_content(url):
+    '''
+    Gets the body contents of a wikipedia page
+    
+    Args:
+        url (str): URL to a wiki page
+    
+    Returns: 
+        bs4 object: bs4 object contained the body content HTML data of the wiki page
+        False: Occurs if the page fails to open or does not exist
+    '''
+    page = open_page(url)
+
+    if page == False:
+        return False
+
+    soup = BeautifulSoup(page, 'html.parser')
+
+    for div in soup.find_all('div', {'class': 'reflist'}):
+        div.decompose()
+
+    soup = soup.find(id='bodyContent')
+
+    return soup
+
+
+def find_links(soup, find_me='', wiki_only=True):
+    '''
+    Finds all links with the find_me keyword in them using Beautiful soup
+    
+    Args:
+        soup (bs4 object): A beautiful soup object
+        find_me (str): Keyword to find. Defaults to find all links.
+        wiki_only (bool): Grabs only the links that start with /wiki/
+    
+    Returns:
+        list: List of links found in the search
+    '''
+    if wiki_only:
+        return [link['href'] for link in soup.find_all('a', href=True, string=find_me) if link['href'].startswith('/wiki/') and ':' not in link['href'] and '#' not in link['href']]
+    else :
+        return [link['href'] for link in soup.find_all('a', href=True, string=find_me) if ':' not in link['href'] and '#' not in link['href']]
 
 
 def open_page(url):
