8a9cdd8e20bdbedeb8e7f0af40d2fd9f623b1ce2
Mitchell Read <fox_mitchell1@hotmail.com>
2019-11-12 20:38:11 -0800

    passed bananas to apples test

--- Wiki_Race/wiki_race.py
+++ Wiki_Race/wiki_race.py
@@ -30,47 +30,63 @@ def wiki_hunt(current_word, end_word, url_attempts):
     }
 
     end_page, end_links = get_wiki_info(end_word)
-    # end_similar = datamuse.get_similar_meaning(end_word)
-    with open('end_sim.json') as f:
-        end_similar = json.load(f)
+    end_similar = datamuse.get_similar_meaning(end_word)
+    # with open('end_sim.json') as f:
+    #     end_similar = json.load(f)
 
     for i in range(url_attempts):
         # Get the current word wiki page links
         curr_page, curr_links = get_wiki_info(current_word)
 
-        if curr_page == end_page:
-            results['successful'] = True 
-            break
+        results = check_winning_page(curr_page, end_page, results)
+
+        if results['successful']:
+            return results
 
         # Look for potential intersections with the target page links
-        # link_intersects = get_link_intersects(curr_links, end_links)
-        with open('same_links.txt') as f:
-            link_intersects = f.read().lower().splitlines()
+        link_intersects = get_link_intersects(curr_links, end_links)
+        # with open('same_links.txt') as f:
+        #     link_intersects = f.read().lower().splitlines()
 
         if link_intersects != []:
             words_similar = find_similar_words(current_word, end_similar, link_intersects)
+            words_similar = remove_visited_pages(results['current_path'], words_similar)
 
             if words_similar:
                 highest_rank = max(words_similar, key=words_similar.get)
 
                 current_word = highest_rank
-                print(current_word)
                 results['current_path'].append(current_word)
                 continue
             
+    return results
 
+
+def check_winning_page(curr_page, end_page, results):
+    if curr_page == end_page:
+        results['successful'] = True
+    elif end_page.title.lower() in [link.lower() for link in curr_page.links]:
+        results['current_path'].append(end_page.title.lower())
+        results['successful'] = True 
 
     return results
+
+def remove_visited_pages(visited_pages, word_rankings):
+    for page in visited_pages:
+        if page in word_rankings:
+            del word_rankings[page]
+
+    return word_rankings
 
 
 def find_similar_words(current_word, end_similar, link_intersects):
     datamuse = Datamuse()
 
     # Get words similar to the start and end
-    # start_similar = datamuse.get_similar_meaning(current_word)
+    start_similar = datamuse.get_similar_meaning(current_word)
 
-    with open('start_sim.json') as f:
-        start_similar = json.load(f)
+    # with open('start_sim.json') as f:
+    #     start_similar = json.load(f)
 
     # Find all the similar words that match with the intersection totalling duplicates scores
     summed_relations = intersect_similar_words(start_similar, end_similar, link_intersects)
@@ -115,8 +131,8 @@ def get_link_intersects(page_one_links, page_two_links):
 
 def get_wiki_info(wiki_title):
     wiki_page = wikipedia.page(title=wiki_title)
-    # links = [link.lower() for link in wiki_page.links]
-    links = ['dfadf']
+    # links = ['dfadf']
+    links = [link.lower() for link in wiki_page.links]
     return (wiki_page, links)
 
 
